9f864cf3b67a2b5b2ed0fcf1007ff255aee6016e
monicalima18666 <monicalima@ipvc.pt>
2022-02-01 21:38:47 +0000

    login

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const { MONGO_URI } = require('./config');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
+    next();
+
+});
+
 // routes 
 
 const usersRoutes = require('./routes/api/users');
